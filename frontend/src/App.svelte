cb55db9be3acda0b824a88de5a701a2c91df383c
mikolaj6r <mikolaj6r@gmail.com>
2020-09-02 13:51:16 +0200

    style: more refactor

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,142 +1,71 @@
 <script>
   import FontList from "./FontList.svelte";
+  import { getHashCode, getFileName, getFonts } from "./utils";
 
-  function getHashCode(string) {
-    let hash = 0,
-      i,
-      chr;
-    for (i = 0; i < string.length; i++) {
-      chr = string.charCodeAt(i);
-      hash = (hash << 5) - hash + chr;
-      hash |= 0;
-    }
-    return hash;
-  }
+  const externalCSSResources = {};
+  const inlineCSSResources = {};
+  const foundDeclarations = {};
+  $: declarations = Object.entries(foundDeclarations);
 
-  const fontRuleRegex = /@font-face\s*{([\s\S]*?)}/g;
-  const declarationsRegex = /(?<property>\S*?):\s*(?<value>[\s\S]+?)(?:;|$)/g;
+  function getStyles(elements, resourcesMap) {
+    if (
+      elements.some((style) => {
+        const hash = getHashCode(style);
+        return !(hash in resourcesMap);
+      })
+    ) {
+      for (let style of elements) {
+        const hash = getHashCode(style);
 
-  let mySidebar;
-  let externalCSSResources = {};
-  let inlineCSSResources = {};
-  let foundDeclarations = {};
-
-  function callback() {
-    chrome.devtools.inspectedWindow.eval(
-      `values({ elements: $$('style').map(el => el.textContent), external: $$('link[rel="stylesheet"]').map(link => link.href) })`,
-      function ([elements, external], isException) {
-        if (external.some((link) => !(link in externalCSSResources))) {
-          for (let link of external) {
-            if (!(link in externalCSSResources)) {
-              externalCSSResources[link] = link;
-            }
-          }
-
-          chrome.devtools.inspectedWindow.getResources((resources) => {
-            for (let stylesheet of Object.keys(externalCSSResources)) {
-              resources
-                .find((res) => res.url == stylesheet)
-                .getContent((content) => {
-                  const fonts = [];
-                  for (const [, declaration] of content.matchAll(
-                    fontRuleRegex
-                  )) {
-                    const declarations = {};
-                    for (const match of declaration.matchAll(
-                      declarationsRegex
-                    )) {
-                      declarations[match.groups.property] = match.groups.value;
-                    }
-                    fonts[fonts.length] = declarations;
-                  }
-
-                  if (fonts.length > 0) {
-                    foundDeclarations[stylesheet] = fonts;
-                    foundDeclarations = foundDeclarations;
-                  }
-                });
-            }
-
-            if (
-              elements.some((style) => {
-                const hash = getHashCode(style);
-
-                return !(hash in inlineCSSResources);
-              })
-            ) {
-              for (let style of elements) {
-                const hash = getHashCode(style);
-
-                if (!(hash in inlineCSSResources)) {
-                  inlineCSSResources[hash] = style;
-                }
-              }
-
-              const inlineStyles = [];
-              const fonts = [];
-
-              for (let style of elements) {
-                for (const [, declaration] of style.matchAll(fontRuleRegex)) {
-                  const declarations = {};
-                  for (const match of declaration.matchAll(declarationsRegex)) {
-                    declarations[match.groups.property] = match.groups.value;
-                  }
-                  fonts[fonts.length] = declarations;
-                }
-
-                if (fonts.length > 0) inlineStyles[inlineStyles.length] = fonts;
-              }
-
-              if (inlineStyles.length > 0) {
-                foundDeclarations["inline"] = inlineStyles;
-                foundDeclarations = foundDeclarations;
-              }
-            }
-          });
-        } else {
-          if (
-            elements.some((style) => {
-              const hash = getHashCode(style);
-              return !(hash in inlineCSSResources);
-            })
-          ) {
-            for (let style of elements) {
-              const hash = getHashCode(style);
-
-              if (!(hash in inlineCSSResources)) {
-                inlineCSSResources[hash] = style;
-              }
-            }
-
-            const inlineStyles = [];
-            const fonts = [];
-
-            for (let style of elements) {
-              for (const [, declaration] of style.matchAll(fontRuleRegex)) {
-                const declarations = {};
-                for (const match of declaration.matchAll(declarationsRegex)) {
-                  declarations[match.groups.property] = match.groups.value;
-                }
-                fonts[fonts.length] = declarations;
-              }
-
-              if (fonts.length > 0) inlineStyles[inlineStyles.length] = fonts;
-            }
-
-            if (inlineStyles.length > 0) {
-              foundDeclarations["inline"] = inlineStyles;
-              foundDeclarations = foundDeclarations;
-            }
-          }
+        if (!(hash in resourcesMap)) {
+          resourcesMap[hash] = style;
         }
       }
-    );
+
+      const inlineStyles = [];
+
+      for (let style of elements) {
+        const fonts = getFonts(style);
+        if (fonts.length > 0) inlineStyles[inlineStyles.length] = fonts;
+      }
+
+      return inlineStyles;
+    }
   }
 
-  function getFileName(string) {
-    const array = string.split("/");
-    return array[array.length - 1];
-  }
+  chrome.devtools.inspectedWindow.eval(
+    `values({ elements: $$('style').map(el => el.textContent), external: $$('link[rel="stylesheet"]').map(link => link.href) })`,
+    function ([elements, external], isException) {
+      const inlineStyles = getStyles(elements, inlineCSSResources);
+      if (inlineStyles.length > 0) {
+        foundDeclarations["inline"] = inlineStyles;
+        foundDeclarations = foundDeclarations;
+      }
+
+      if (external.some((link) => !(link in externalCSSResources))) {
+        for (let link of external) {
+          if (!(link in externalCSSResources)) {
+            externalCSSResources[link] = link;
+          }
+        }
+
+        chrome.devtools.inspectedWindow.getResources((resources) => {
+          for (let stylesheet of Object.keys(externalCSSResources)) {
+            resources
+              .find((res) => res.url == stylesheet)
+              .getContent((content) => {
+                const fonts = getFonts(content);
+
+                if (fonts.length > 0) {
+                  foundDeclarations[stylesheet] = fonts;
+                  foundDeclarations = foundDeclarations;
+                }
+              });
+          }
+        });
+      }
+    }
+  );
 
   function inlineSourceClicked(event) {
     const { index } = event.currentTarget.dataset;
@@ -155,10 +84,6 @@
 
     chrome.devtools.panels.openResource(url);
   }
-
-  callback();
-
-  $: declarations = Object.entries(foundDeclarations);
 </script>
 
 <style>
---
+++ frontend/src/utils.js
@@ -0,0 +1,31 @@
+export function getHashCode(string) {
+  let hash = 0,
+    i,
+    chr;
+  for (i = 0; i < string.length; i++) {
+    chr = string.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    hash |= 0;
+  }
+  return hash;
+}
+
+export function getFileName(string) {
+  const array = string.split("/");
+  return array[array.length - 1];
+}
+
+export const fontRuleRegex = /@font-face\s*{([\s\S]*?)}/g;
+export const declarationsRegex = /(?<property>\S*?):\s*(?<value>[\s\S]+?)(?:;|$)/g;
+
+export function getFonts(content) {
+  const fonts = [];
+  for (const [, declaration] of content.matchAll(fontRuleRegex)) {
+    const declarations = {};
+    for (const match of declaration.matchAll(declarationsRegex)) {
+      declarations[match.groups.property] = match.groups.value;
+    }
+    fonts[fonts.length] = declarations;
+  }
+  return fonts;
+}
